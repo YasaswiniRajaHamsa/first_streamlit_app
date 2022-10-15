6bd723179a36437cab17798b0a9eac28c771250b
Yasaswini Raja Hamsa <115868072+YasaswiniRajaHamsa@users.noreply.github.com>
2022-10-15 15:25:43 +0530

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.text('🥑🍞Blueberry oatmeal')
 streamlit.text('idly')
 streamlit.text('dosa')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-
-import streamlit
 streamlit.dataframe(my_fruit_list)
