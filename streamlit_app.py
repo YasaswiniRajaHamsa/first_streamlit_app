0798f54d765ede81eee4fb6b8af2e030fb87ce62
Yasaswini Raja Hamsa <115868072+YasaswiniRajaHamsa@users.noreply.github.com>
2022-10-15 14:47:07 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breafast menu')
+streamlit.text('Blueberry oatmeal')
+streamlit.text('idly')
+streamlit.text('dosa')
