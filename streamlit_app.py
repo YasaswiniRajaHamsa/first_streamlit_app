409826c04d184a973344627d86cce116eb43ecde
Yasaswini Raja Hamsa <115868072+YasaswiniRajaHamsa@users.noreply.github.com>
2022-10-16 15:24:58 +0530

    Update streamlit_app.py
    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,12 @@ streamlit.text('🥑🍞Blueberry oatmeal')
 streamlit.text('idly with ghee')
 streamlit.text('dosa with podi')
 
-
-
 streamlit.header('Build your own fruit smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-
-streamlit.stop()
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -29,6 +25,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 
 import snowflake.connector
